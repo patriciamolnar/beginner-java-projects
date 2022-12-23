3ad4ca7ba288857ce5602eefb6d6924e6d1f4bc9
Patricia Molnar <patricia.molnar@universitycompare.com>
2022-12-23 12:58:27 +0000

    [calculator] - adding Multiplication & Division classes

--- calculator/src/main/java/com/patriciamolnar/App.java
+++ calculator/src/main/java/com/patriciamolnar/App.java
@@ -11,5 +11,13 @@ public class App
         Subtraction subtraction = new Subtraction(); 
         result = subtraction.subtract(12, 5);
         System.out.println("The subtraction's result is: " + result);
+
+        Multiplication multiplication = new Multiplication(); 
+        result = multiplication.multiply(5, 2);
+        System.out.println("The subtraction's result is: " + result);
+
+        Division division = new Division(); 
+        result = division.divide(10, 4); 
+        System.out.println("The subtraction's result is: " + result);
     }
 }
