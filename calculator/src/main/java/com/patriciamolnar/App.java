a8dd5570ebee1f30a4c06b9d25d959ed0dddf1ce
Patricia Molnar <patricia.molnar@universitycompare.com>
2022-12-23 12:44:12 +0000

    [java] - adding Subtraction class

--- calculator/src/main/java/com/patriciamolnar/App.java
+++ calculator/src/main/java/com/patriciamolnar/App.java
@@ -6,6 +6,10 @@ public class App
     {
         Addition addition = new Addition(); 
         int result = addition.add(12, 5); 
-        System.out.println("The result is: " + result);
+        System.out.println("The addition's result is: " + result);
+
+        Subtraction subtraction = new Subtraction(); 
+        result = subtraction.subtract(12, 5);
+        System.out.println("The subtraction's result is: " + result);
     }
 }
---
+++ calculator/src/main/java/com/patriciamolnar/Subtraction.java
@@ -0,0 +1,7 @@
+package com.patriciamolnar;
+
+public class Subtraction {
+    int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+}
