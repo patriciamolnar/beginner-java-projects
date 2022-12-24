10406d04b0536c2769d356d59326d8f6e3284004
Patricia Molnar <patricia.molnar@universitycompare.com>
2022-12-24 08:38:16 +0000

    [addition] - rewriting to take user input

--- calculator/src/main/java/com/patriciamolnar/Addition.java
+++ calculator/src/main/java/com/patriciamolnar/Addition.java
@@ -1,7 +1,15 @@
 package com.patriciamolnar;
 
+import java.util.Scanner;
+
 public class Addition {
-    int add(int num1, int num2) {
-        return num1 + num2; 
+    public static void main(String[] args) {
+        System.out.println("Please input first number and press enter:");
+        Scanner input = new Scanner(System.in); 
+        float a = input.nextFloat(); 
+        System.out.println("Please input second number and press enter:");
+        float b = input.nextFloat();
+        input.close();
+        System.out.println("The sum of " + a + " + " + b + " = " + Float.sum(a, b));
     }
 }
