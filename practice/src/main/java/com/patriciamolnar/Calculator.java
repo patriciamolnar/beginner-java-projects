f0f5683eb53f0b8ec26513ea802408525fcbbb5a
Patricia Molnar <patricia.molnar@universitycompare.com>
2022-12-25 17:08:52 +0000

    Adding ANSI colours and enhancing Calculator with colours in command line

--- practice/src/main/java/com/patriciamolnar/Calculator.java
+++ practice/src/main/java/com/patriciamolnar/Calculator.java
@@ -10,10 +10,12 @@ public class Calculator {
         Calculator calculator = new Calculator(); 
 
         try {
-            System.out.println("Welcome to Patricia's first Java Calculater!");
-            System.out.println("Please enter your 1st number:");
+            System.out.println(Ansi.ANSI_PURPLE + "===============================================" + Ansi.ANSI_RESET);
+            System.out.println(Ansi.ANSI_PURPLE + "Welcome to Patricia's first Java Calculater! ♥" + Ansi.ANSI_RESET);
+            System.out.println(Ansi.ANSI_PURPLE + "===============================================" + Ansi.ANSI_RESET);
+            System.out.println(Ansi.ANSI_BLUE + "Please enter your 1st number:" + Ansi.ANSI_RESET);
             float x = scanner.nextFloat(); 
-            System.out.println("Thanks! Please enter the 2nd number:");
+            System.out.println(Ansi.ANSI_BLUE + "Thanks! Please enter the 2nd number:" + Ansi.ANSI_RESET);
             float y = scanner.nextFloat(); 
             do {
                 calculator.printOptions(operand);
@@ -21,10 +23,10 @@ public class Calculator {
             } while (operand < 0 || operand > 5);
             
             String result = calculator.calculate(x, y, operand);
-            System.out.println(result);
+            System.out.println(Ansi.ANSI_PURPLE + result + Ansi.ANSI_RESET);
         } 
         catch (InputMismatchException ime) {
-            System.out.println("You messed up! I'm gonna quit on you! Bye...");
+            System.out.println(Ansi.ANSI_RED + "You messed up! I'm gonna quit on you! Bye..." + Ansi.ANSI_RESET);
         }
         finally {
             scanner.close();
@@ -32,39 +34,39 @@ public class Calculator {
     }
 
     void printOptions(int operand) {
-        System.out.println("Now please choose one of the following operations");
-        System.out.println("Press 1 for ADDITION");
-        System.out.println("Press 2 for SUBTRACTION");
-        System.out.println("Press 3 for MULTIPLICATION");
-        System.out.println("Press 4 for DIVISION");
-        System.out.println("Press 5 for MODULUS");
+        System.out.println(Ansi.ANSI_BLUE + "Now please choose one of the following operations" + Ansi.ANSI_RESET);
+        System.out.println("Press + " + Ansi.ANSI_BLUE + "1" + Ansi.ANSI_RESET + " for " + Ansi.ANSI_BLUE + "addition" + Ansi.ANSI_RESET);
+        System.out.println("Press + " + Ansi.ANSI_BLUE + "2" + Ansi.ANSI_RESET + " for " + Ansi.ANSI_BLUE + "subtraction" + Ansi.ANSI_RESET);
+        System.out.println("Press + " + Ansi.ANSI_BLUE + "3" + Ansi.ANSI_RESET + " for " + Ansi.ANSI_BLUE + "multiplication" + Ansi.ANSI_RESET);
+        System.out.println("Press + " + Ansi.ANSI_BLUE + "4" + Ansi.ANSI_RESET + " for " + Ansi.ANSI_BLUE + "division" + Ansi.ANSI_RESET);
+        System.out.println("Press + " + Ansi.ANSI_BLUE + "5" + Ansi.ANSI_RESET + " for " + Ansi.ANSI_BLUE + "modulus" + Ansi.ANSI_RESET);
     }
 
     String calculate(float x, float y, int operand) {
         String result;
         switch(operand) {
             case 1: {
-                result = "The result of " + x + " + " + y + " is " + Float.sum(x, y); 
+                result = "The result of " + x + " + " + y + " is " + Float.sum(x, y) + "."; 
                 break;
             }
             case 2: {
-                result = "The result of " + x + " - " + y + " is " + (x - y); 
+                result = "The result of " + x + " - " + y + " is " + (x - y) + "."; 
                 break;
             }
             case 3: {
-                result = "The result of " + x + " * " + y + " is " + (x * y); 
+                result = "The result of " + x + " * " + y + " is " + (x * y) + "."; 
                 break;
             }
             case 4: {
-                result = "The result of " + x + " / " + y + " is " + (x / y); 
+                result = "The result of " + x + " / " + y + " is " + (x / y) + "."; 
                 break;
             }
             case 5: {
-                result = "The result of " + x + " % " + y + " is " + (x % y);
+                result = "The result of " + x + " % " + y + " is " + (x % y) + ".";
                 break;
             }
             default: {
-                result = "The result of " + x + " + " + y + " is " + (x + y);
+                result = "The result of " + x + " + " + y + " is " + (x + y) + ".";
             }
         }
 
