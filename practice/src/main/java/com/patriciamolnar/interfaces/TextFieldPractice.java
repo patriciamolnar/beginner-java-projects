a89c07716e0471456106075705150b62913aad2a
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-01-03 17:04:48 +0000

    adding basic TextField practice file

---
+++ practice/src/main/java/com/patriciamolnar/interfaces/TextFieldPractice.java
@@ -0,0 +1,21 @@
+package com.patriciamolnar.interfaces;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class TextFieldPractice {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Example");
+        JTextField field = new JTextField("Hello world!"); 
+
+        field.setBackground(Color.PINK);
+        field.setBounds(50, 35, 200, 50);
+
+        frame.add(field);
+        frame.setSize(300, 150);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
