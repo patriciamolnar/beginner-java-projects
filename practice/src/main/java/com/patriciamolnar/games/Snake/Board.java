34055d77626bf8e2a8c0145b4479dee75a58c09f
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-01-07 15:25:10 +0000

    snake game complete

--- practice/src/main/java/com/patriciamolnar/games/Snake/Board.java
+++ practice/src/main/java/com/patriciamolnar/games/Snake/Board.java
@@ -42,11 +42,12 @@ public class Board extends JPanel implements ActionListener {
 
     private void initBoard() {
         addKeyListener(new TAdapter());
-    }
+        setBackground(Color.black);
+        setFocusable(true);
 
-    public static void main(String[] args) {
-        Board board = new Board(); 
-        board.loadImages(); 
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
+        loadImages();
+        initGame();
     }
 
     private void loadImages() {
@@ -58,14 +59,131 @@ public class Board extends JPanel implements ActionListener {
         head = iih.getImage(); 
     }
 
+    private void initGame() {
+        dots = 3; 
+        for(int z = 0; z < dots; z++) {
+            x[z] = 50 - z * 10; 
+            y[z] = 50;
+        }
+
+        locateApple();
+
+        timer = new Timer(DELAY, this); 
+        timer.start(); 
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+    }
+
+    private void doDrawing(Graphics g) {
+        if(inGame) {
+            g.drawImage(apple, apple_x, apple_y, this);
+            
+            for(int z = 0; z < dots; z ++) {
+                if(z == 0) {
+                    g.drawImage(head, x[z], y[z], this); 
+                } else {
+                    g.drawImage(dot, x[z], y[z], this);
+                }
+            }
+
+            Toolkit.getDefaultToolkit().sync();
+        } else {
+            gameOver(g);
+        }
+    }
+
+    private void gameOver(Graphics g) {
+        String msg = "Game Over"; 
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = getFontMetrics(small); 
+
+        g.setColor(Color.white);
+        g.setFont(small); 
+        g.drawString(msg, (BOARD_WIDTH - metr.stringWidth(msg)) / 2, BOARD_HEIGHT / 2);
+    }
+
+    private void checkApple() {
+        if((x[0] == apple_x) && (y[0] == apple_y)) {
+            dots++;
+            locateApple();
+        }
+    }
+
+    private void move() {
+        for(int z = dots; z > 0; z--) {
+            x[z] = x[(z - 1)];
+            y[z] = y[(z - 1)];
+        }
+
+        if(leftDir) {
+            x[0] -= DOT_SIZE;
+        }
+
+        if (rightDir) {
+            x[0] += DOT_SIZE;
+        }
+
+        if (upDir) {
+            y[0] -= DOT_SIZE;
+        }
+
+        if (downDir) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    private void checkCollision() {
+
+        for (int z = dots; z > 0; z--) {
+
+            if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+                inGame = false;
+            }
+        }
+
+        if (y[0] >= BOARD_HEIGHT) {
+            inGame = false;
+        }
+
+        if (y[0] < 0) {
+            inGame = false;
+        }
+
+        if (x[0] >= BOARD_WIDTH) {
+            inGame = false;
+        }
+
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        
+        if (!inGame) {
+            timer.stop();
+        }
+    }
+
+
+    private void locateApple() {
+        int r = (int) (Math.random() * RAND_POS); 
+        apple_x = ((r * DOT_SIZE));
+
+        r = (int) (Math.random() * RAND_POS);
+        apple_y = ((r * DOT_SIZE));
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
         if (inGame) {
             System.out.println("In Game");
-            // checkApple();
-            // checkCollision();
-            // move();
+            checkApple();
+            checkCollision();
+            move();
         }
 
         repaint();
@@ -76,6 +194,30 @@ public class Board extends JPanel implements ActionListener {
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode(); 
             System.out.println(key);
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDir)) {
+                leftDir = true;
+                upDir = false;
+                downDir = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDir)) {
+                rightDir = true;
+                upDir = false;
+                downDir = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDir)) {
+                upDir = true;
+                rightDir = false;
+                leftDir = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDir)) {
+                downDir = true;
+                rightDir = false;
+                leftDir = false;
+            }
         }
     }
 }
--- practice/src/main/java/com/patriciamolnar/games/Snake/Snake.java
+++ practice/src/main/java/com/patriciamolnar/games/Snake/Snake.java
@@ -1,6 +1,29 @@
 package com.patriciamolnar.games.Snake;
 
-public class Snake {
-    
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+    public Snake() {
+        initUI(); 
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setResizable(false);
+        pack(); 
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
 
 }
