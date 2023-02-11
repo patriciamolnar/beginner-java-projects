b37cee128f514d3c07df400c464f368a78733ff1
Patricia Molnar <patricia.molnar@universitycompare.com>
2023-02-11 13:20:55 +0000

    adding conversion to different time zones

--- practice/src/main/java/com/patriciamolnar/WorldClock.java
+++ practice/src/main/java/com/patriciamolnar/WorldClock.java
@@ -2,7 +2,10 @@ package com.patriciamolnar;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 
 public class WorldClock {
@@ -18,7 +21,19 @@ public class WorldClock {
             String timeInput = input.nextLine();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/d HH:mm");
             LocalDateTime time = LocalDateTime.parse(timeInput, formatter);
-            System.out.println(time);
+            // ZonedDateTime current = ZonedDateTime.of(time, ZoneId.systemDefault()); 
+            // ZonedDateTime hongKong = ZonedDateTime.of(time, ZoneId.of("Hongkong"));
+            // ZonedDateTime shangHai = ZonedDateTime.of(time, ZoneId.of("Asia/Shanghai"));
+            // ZonedDateTime losAngeles = ZonedDateTime.of(time, ZoneId.of("America/Los_Angeles"));
+            // ZonedDateTime berlin = ZonedDateTime.of(time, ZoneId.of("Europe/Berlin"));
+            // ZonedDateTime moscow = ZonedDateTime.of(time, ZoneId.of("Europe/Moscow"));
+
+            System.out.println("You're current time is " + time);
+            System.out.println("In 香港, that's: " + time.atZone(ZoneId.of("Hongkong")));
+            // System.out.println("In 上海, that's: " + shangHai);
+            // System.out.println("In Los Angeles, that's: " + losAngeles);
+            // System.out.println("In Berlin, that's: " + berlin);
+            // System.out.println("In Moscow, that's: " + moscow);
         } catch(Exception ex) {
             System.out.println(ex.getMessage());
         }
